64e38ec5f5a3dfc5293aa2a079402c33bd06b675
ikrotinho02 <ikrotinho02@gmail.com>
2025-12-26 13:00:29 -0300

    main.tsx

--- main.tsx
+++ main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import './index.css' // Se você tiver um arquivo de estilo, se não tiver pode apagar essa linha
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
